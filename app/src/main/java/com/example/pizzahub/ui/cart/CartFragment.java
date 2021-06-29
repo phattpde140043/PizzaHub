ffaeb13e8a2abc250f9fee13f6db79e600a6185c
yetngo <pebitrontron@gmail.com>
2021-06-29 22:19:04 +0700

    change dashboard to cart and temporary fix bug in home

--- app/src/main/java/com/example/pizzahub/ui/cart/CartFragment.java
+++ app/src/main/java/com/example/pizzahub/ui/cart/CartFragment.java
@@ -1,35 +1,31 @@
-package com.example.pizzahub.ui.home;
+package com.example.pizzahub.ui.cart;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.pizzahub.R;
-import com.example.pizzahub.databinding.FragmentHomeBinding;
+import com.example.pizzahub.databinding.FragmentCartBinding;
 
-public class HomeFragment extends Fragment {
+public class CartFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
+    private CartViewModel cartViewModel;
+    private FragmentCartBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        cartViewModel =
+                new ViewModelProvider(this).get(CartViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentCartBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//        final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
 //                textView.setText(s);
--- app/src/main/java/com/example/pizzahub/ui/cart/CartViewModel.java
+++ app/src/main/java/com/example/pizzahub/ui/cart/CartViewModel.java
@@ -1,16 +1,16 @@
-package com.example.pizzahub.ui.dashboard;
+package com.example.pizzahub.ui.cart;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class CartViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public CartViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is cart fragment");
     }
 
     public LiveData<String> getText() {
