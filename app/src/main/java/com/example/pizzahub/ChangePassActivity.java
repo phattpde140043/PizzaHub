dc0298df81de32dfa63da4825d4341e3b4a19a02
phattpde140043 <pinn.b18pct@gmail.com>
2021-07-20 00:20:52 +0700

    Change pass 50 %. Còn lại đang fix

--- app/src/main/java/com/example/pizzahub/ChangePassActivity.java
+++ app/src/main/java/com/example/pizzahub/ChangePassActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,11 @@ import androidx.constraintlayout.widget.ConstraintSet;
 
 public class ChangePassActivity extends AppCompatActivity {
 
+    Button Close_btn;
+    Button Save_btn;
+    EditText CurrPass_txt;
+    EditText NewPass_txt;
+    EditText CfrNewPass_txt;
 
 
 
@@ -19,6 +25,34 @@ public class ChangePassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_password);
+        Save_btn = (Button) findViewById(R.id.SaveButton);
+        Close_btn = (Button) findViewById(R.id.CloseButton);
+        CurrPass_txt= (EditText) findViewById(R.id.curPass_txt);
+        NewPass_txt= (EditText) findViewById(R.id.newPass_txt);
+        CfrNewPass_txt=(EditText) findViewById(R.id.cfrPass_txt);
+
+        Close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.putExtra("Fragment_number",3);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.putExtra("Fragment_number",3);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
